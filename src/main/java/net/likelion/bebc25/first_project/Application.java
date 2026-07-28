9a3c362c9f35794270359fe9053591b4b71dc2ee
Byeongho Kim <qudgh0217@naver.com>
2026-07-28 15:00:41 +0900

    rename: 프로젝트 디렉토리명 수정

--- src/main/java/net/likelion/bebc25/first_project/Application.java
+++ src/main/java/net/likelion/bebc25/first_project/Application.java
@@ -1,4 +1,4 @@
-package net.likelion.bebc25._is_first_project;
+package net.likelion.bebc25.first_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
