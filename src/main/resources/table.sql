a539d63777f91846df89a9db201038706e0c4b4d
Byeongho Kim <qudgh0217@naver.com>
2026-07-28 11:47:31 +0900

    chore: db: db 테이블명 수정 및 일부 column명 수정

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -11,18 +11,18 @@ CREATE TABLE IF NOT EXISTS member
 # DROP TABLE IF EXISTS post;
 CREATE TABLE IF NOT EXISTS post
 (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    member_id   VARCHAR(20) REFERENCES member (id) ON DELETE RESTRICT,
-    game_id     int REFERENCES game (id) ON DELETE CASCADE NOT NULL,
-    title       VARCHAR(30)                                NOT NULL,
-    content     TEXT                                       NOT NULL,
-    max_count   INT      DEFAULT 0,
-    reply_count INT      DEFAULT 0,
-    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP
+    id                INT AUTO_INCREMENT PRIMARY KEY,
+    member_id         VARCHAR(20) REFERENCES member (id) ON DELETE RESTRICT,
+    game_id           int REFERENCES game (id) ON DELETE CASCADE NOT NULL,
+    title             VARCHAR(30)                                NOT NULL,
+    content           TEXT                                       NOT NULL,
+    max_count         INT      DEFAULT 0,
+    participant_count INT      DEFAULT 0,
+    created_at        DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-# DROP TABLE IF EXISTS reply;
-CREATE TABLE IF NOT EXISTS reply
+# DROP TABLE IF EXISTS user_info;
+CREATE TABLE IF NOT EXISTS user_info
 (
     id         INT AUTO_INCREMENT PRIMARY KEY,
     member_id  VARCHAR(20) REFERENCES member (id) ON DELETE RESTRICT,
