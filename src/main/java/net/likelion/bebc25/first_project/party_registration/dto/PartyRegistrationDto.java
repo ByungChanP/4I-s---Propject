d9b2437ce16a9c5afabf9c91647ec746915c2123
Byeongho Kim <qudgh0217@naver.com>
2026-07-29 15:00:17 +0900

    feat: PartyRegistrationDto 객체 구현

--- src/main/java/net/likelion/bebc25/first_project/party_registration/dto/PartyRegistrationDto.java
+++ src/main/java/net/likelion/bebc25/first_project/party_registration/dto/PartyRegistrationDto.java
@@ -2,6 +2,8 @@ package net.likelion.bebc25.first_project.party_registration.dto;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -10,5 +12,13 @@ import lombok.*;
 @Builder
 // 게시글 하나를 저장할 객체
 public class PartyRegistrationDto {
+    private int id;
 
+    private int postId;
+
+    private String author;
+
+    private int content;
+
+    private LocalDateTime createdAt;
 }
