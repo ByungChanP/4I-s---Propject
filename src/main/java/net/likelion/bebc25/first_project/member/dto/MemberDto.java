f7dff2d3cc2001dd80c2648a0a2b2b0bd4aa275e
Byeongho Kim <qudgh0217@naver.com>
2026-07-29 14:53:42 +0900

    feat: MemberDto 객체 구현

--- src/main/java/net/likelion/bebc25/first_project/member/dto/MemberDto.java
+++ src/main/java/net/likelion/bebc25/first_project/member/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package net.likelion.bebc25.first_project.member.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -23,26 +24,30 @@ public class MemberDto {
     private int id;
 
     /**
-     * 회원 로그인 아이디
+     * 회원 로그인 아이디 -> 이메일로 통합
      */
-    @NotBlank(message = "아이디는 필수 입력 항목입니다.")
-    @Size(min = 4, max = 50, message = "아이디는 4자 이상 50자 이하여야 합니다.")
-    private String username;
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    @Email(message = "올바르지 않은 이메일 형식입니다.")
+    private String email;
 
     /**
      * 회원 로그인 비밀번호
      */
     @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
-    @Size(min = 4, max = 100, message = "비밀번호는 4자 이상 100자 이하여야 합니다.")
+    @Size(min = 4, max = 20, message = "비밀번호는 4자 이상 20자 이하여야 합니다.")
     private String password;
 
-    /**
-     * 회원 이메일 주소
-     */
-    private String email;
+    @NotBlank
+    @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하여야 합니다.")
+    private String nickname;
 
     /**
      * 회원 가입 일시
      */
     private LocalDateTime createdAt;
+
+    /**
+     * 프로필 이미지 저장경로
+     */
+    private String profileImgDir;
 }
