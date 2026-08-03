dc94178743ecd3540c4036754afb63a4fdd12654
ByungChanP <qudcks4095@gmail.com>
2026-08-03 22:18:31 +0900

    user_game_info

---
+++ src/main/java/net/likelion/bebc25/first_project/user_game_info/repository/UserGameInfoRepository.java
@@ -0,0 +1,30 @@
+package net.likelion.bebc25.first_project.user_game_info.repository;
+
+import net.likelion.bebc25.first_project.user_game_info.dto.InfoDto;
+
+import java.util.List;
+
+
+public interface UserGameInfoRepository {
+
+    /**
+     * 새로운 인게임 정보를 저장합니다.
+     * @param gameInfo 저장할 인게임 정보  DTO
+     */
+    void save(InfoDto gameInfo);
+
+    /**
+     * 기존 인게임 정보를 수정합니다
+     * @param gameInfo 수정할 인게임 정보 객체
+     */
+    void update(InfoDto gameInfo);
+
+    /**
+     * 프로필에 등록한 인게임 정보를 조회
+     * @param id 해당 게임 ID
+     * @param id 해당 유저 ID
+     * @return
+     */
+    List<InfoDto> findByID(int id);
+
+}
