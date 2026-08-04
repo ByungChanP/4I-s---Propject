e07dc1b03bd1214004bea34f4fb308a0e04a52f4
Byeongho Kim <qudgh0217@naver.com>
2026-08-04 18:13:01 +0900

    feat:login error 수정완료

--- src/main/java/net/likelion/bebc25/first_project/member/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/net/likelion/bebc25/first_project/member/repository/JdbcTemplateMemberRepository.java
@@ -1,6 +1,8 @@
 package net.likelion.bebc25.first_project.member.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import net.likelion.bebc25.first_project.member.dto.MemberDto;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -14,6 +16,7 @@ import java.util.List;
  * Spring의 JdbcTemplate을 사용하여 회원 데이터를 처리하는 저장소 구현체입니다.
  */
 @Repository
+@Slf4j
 public class JdbcTemplateMemberRepository implements MemberRepository {
 
     private final JdbcTemplate jdbcTemplate;
@@ -47,7 +50,13 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
 
     @Override
     public MemberDto findByEmail(String email) {
-        return jdbcTemplate.queryForObject("SELECT * FROM member WHERE email = ?", memberRowMapper, email);
+        MemberDto member;
+        try {
+            member = jdbcTemplate.queryForObject("SELECT * FROM member WHERE email = ?", memberRowMapper, email);
+        } catch (EmptyResultDataAccessException e) {
+            member = null;
+        }
+        return member;
     }
 
 
