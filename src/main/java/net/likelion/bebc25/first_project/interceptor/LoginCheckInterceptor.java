9b5e6ca43fb262a9e078a8cca6d17da433484077
ByungChanP <qudcks4095@gmail.com>
2026-08-04 17:14:27 +0900

    interceptor bug fix

--- src/main/java/net/likelion/bebc25/first_project/interceptor/LoginCheckInterceptor.java
+++ src/main/java/net/likelion/bebc25/first_project/interceptor/LoginCheckInterceptor.java
@@ -3,7 +3,6 @@ package net.likelion.bebc25.first_project.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import jakarta.validation.constraints.Null;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -19,7 +18,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         // -기본 세션이 없으면 생성하지 않고 null값 반환
         HttpSession session = request.getSession(false);
         // 3. 미인증 사용자 검증
-        if(session == null || session.getAttribute("loginmembver") == null){
+        if(session == null || session.getAttribute("loginMember") == null){
             log.info("로그인 안된 사용자의 요청: " + requestUri);
             // 미인증인 사용자일 경우 로그인 페이지로 다이렉트 요청
             response.sendRedirect("/member/login");
