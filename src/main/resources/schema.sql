0a7c3d77f378e9b75f73f7f30c248f9ac4501824
ByungChanP <qudcks4095@gmail.com>
2026-08-04 15:45:55 +0900

    "schema uique 추가, 문서 일부 수정"

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS member
     id              int AUTO_INCREMENT PRIMARY KEY,
     email           VARCHAR(50) UNIQUE NOT NULL,
     pw              VARCHAR(20)        NOT NULL,
-    nickname        VARCHAR(20)        NOT NULL,
+    nickname        VARCHAR(20) UNIQUE NOT NULL,
     created_at      DATETIME     DEFAULT CURRENT_TIMESTAMP,
     profile_img_dir VARCHAR(255) DEFAULT '/images/icon/person-circle.svg'
 );
